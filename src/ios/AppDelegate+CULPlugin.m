e2a7f39a6cb46557c9b79169cd3a7b2cb57c0fc3
delianides <github@delianides.com>
2017-05-24 16:13:56 -0400

    added hack in to older (possibly) npm version

--- src/ios/AppDelegate+CULPlugin.m
+++ src/ios/AppDelegate+CULPlugin.m
@@ -14,19 +14,24 @@ static NSString *const PLUGIN_NAME = @"UniversalLinks";
 
 @implementation AppDelegate (CULPlugin)
 
-- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *))restorationHandler {
-    // ignore activities that are not for Universal Links
-    if (![userActivity.activityType isEqualToString:NSUserActivityTypeBrowsingWeb] || userActivity.webpageURL == nil) {
-        return NO;
-    }
-    
-    // get instance of the plugin and let it handle the userActivity object
-    CULPlugin *plugin = [self.viewController getCommandInstance:PLUGIN_NAME];
-    if (plugin == nil) {
-        return NO;
-    }
-    
-    return [plugin handleUserActivity:userActivity];
-}
+// XXX HACK
+// iOS only lets you overwrite this method once in an applications. Since we are also using dynamic links, we disable
+// the binding here so it can be used in the FCMPlugin. The FCMPlugin imports the CULPlugin to still handle dynamic links
+
+
+// - (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *))restorationHandler {
+//     // ignore activities that are not for Universal Links
+//     if (![userActivity.activityType isEqualToString:NSUserActivityTypeBrowsingWeb] || userActivity.webpageURL == nil) {
+//         return NO;
+//     }
+
+//     // get instance of the plugin and let it handle the userActivity object
+//     CULPlugin *plugin = [self.viewController getCommandInstance:PLUGIN_NAME];
+//     if (plugin == nil) {
+//         return NO;
+//     }
+
+//     return [plugin handleUserActivity:userActivity];
+// }
 
 @end
